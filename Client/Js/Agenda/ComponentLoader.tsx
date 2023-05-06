60b288d7772fec5c3c6b3616d705db5b0fba4123
Peter Donker <peter@bring2mind.net>
2023-05-06 06:12:16 -0300

    Step 2b: Include a page hop

--- Client/Js/Agenda/ComponentLoader.tsx
+++ Client/Js/Agenda/ComponentLoader.tsx
@@ -6,12 +6,21 @@ import Agenda from "./Components/Agenda";
 declare global {
   interface Element {
     dataInt: (prop: string) => number;
+    dataString: (prop: string, defaultValue: string) => string;
   }
 }
 
 Element.prototype.dataInt = function (this: Element, prop: string): number {
   if (this.getAttribute("data-" + prop) == null) return 0;
   return parseInt(this.getAttribute("data-" + prop) as string);
+};
+Element.prototype.dataString = function (
+  this: Element,
+  prop: string,
+  defaultValue: string
+): string {
+  if (this.getAttribute("data-" + prop) == null) return defaultValue;
+  return this.getAttribute("data-" + prop) as string;
 };
 
 export class ComponentLoader {
@@ -20,6 +29,7 @@ export class ComponentLoader {
       createRoot(el).render(
         <Agenda
           moduleId={el.dataInt("moduleid")}
+          detailUrl={el.dataString("detailurl", "")}
         />
       );
     });
--- Client/Js/Agenda/Components/Agenda.tsx
+++ Client/Js/Agenda/Components/Agenda.tsx
@@ -2,13 +2,19 @@ import * as React from 'react';
 
 interface IAgendaProps {
   moduleId: number;
+  detailUrl: string;
 };
 
 const Agenda: React.FC<IAgendaProps> = props => {
   return (
     <div className="container-fluid">
       <div className="row">
-        Hello React {props.moduleId}
+        <div className="col-12">
+          <h1>Agenda</h1>
+          <p>
+            <a href={props.detailUrl.replace("-1", "1")}>Detail Example</a>
+          </p>
+        </div>
       </div>
     </div>
   );
