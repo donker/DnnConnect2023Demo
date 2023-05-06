d5b22c29d9da6977dd3420ace86ae98df6714f55
Peter Donker <peter@bring2mind.net>
2023-05-06 06:04:29 -0300

    Step 2a: hook in simple values

--- Client/Js/Agenda/ComponentLoader.tsx
+++ Client/Js/Agenda/ComponentLoader.tsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 
 import Agenda from "./Components/Agenda";
 
+declare global {
+  interface Element {
+    dataInt: (prop: string) => number;
+  }
+}
+
+Element.prototype.dataInt = function (this: Element, prop: string): number {
+  if (this.getAttribute("data-" + prop) == null) return 0;
+  return parseInt(this.getAttribute("data-" + prop) as string);
+};
+
 export class ComponentLoader {
   public static load(): void {
     document.querySelectorAll(".Agenda").forEach((el) => {
       createRoot(el).render(
         <Agenda
+          moduleId={el.dataInt("moduleid")}
         />
       );
     });
--- Client/Js/Agenda/Components/Agenda.tsx
+++ Client/Js/Agenda/Components/Agenda.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 
 interface IAgendaProps {
+  moduleId: number;
 };
 
 const Agenda: React.FC<IAgendaProps> = props => {
   return (
     <div className="container-fluid">
       <div className="row">
-        Hello React
+        Hello React {props.moduleId}
       </div>
     </div>
   );
